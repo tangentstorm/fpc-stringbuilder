b05cfd0d72dd5d9b9073b5992fe33a94d00a69a4
sabren <michal.wallace@gmail.com>
2013-09-23 09:00:53 -0500

    copied method signatures from the embarcadero site and got a clean compile with stub methods.

---
+++ ustringbuilder.pas
@@ -0,0 +1,17 @@
+{$mode objfpc}{$H+}
+unit ustringbuilder;
+
+interface
+
+uses SysUtils;
+
+type TCharArray = array of char;
+
+{$i stringbuilderh.inc}
+
+implementation
+
+{$i stringbuilder.inc}
+
+end.
+
